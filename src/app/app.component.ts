64e20d6d7e0289816029f1c821ff88c9d964a28e
Wassem Keddah <wassemkeddah@gmail.com>
2019-06-11 10:25:45 +0200

    test pdf orientation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ExportAsService, ExportAsConfig } from 'ngx-export-as';
+import { ExportAsService, ExportAsConfig, SupportedExtensions } from 'ngx-export-as';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +11,22 @@ export class AppComponent {
   config: ExportAsConfig = {
     type: 'pdf',
     elementId: 'mytable',
+    options: {
+      jsPDF: {
+        orientation: 'landscape'
+      }
+    }
   };
 
   constructor(
     private exportAsService: ExportAsService
-  ) {}
+  ) { }
 
-  exportAs(type) {
+  exportAs(type: SupportedExtensions, opt?: string) {
     this.config.type = type;
+    if (opt) {
+      this.config.options.jsPDF.orientation = opt;
+    }
     this.exportAsService.save(this.config, 'myFile');
     // this.exportAsService.get(this.config).subscribe(content => {
     //   console.log(content);
